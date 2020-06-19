6fa9f0bef88ab49eb22bfda8aa13c41c07127fc6
DURANT <durant@meerketeer.ird.fr>
2020-06-19 15:24:36 +0000

    Cleaned MatrixPavZoom.vue

--- poke_cli_app/src/components/MatrixPavZoom.vue
+++ poke_cli_app/src/components/MatrixPavZoom.vue
@@ -4,9 +4,24 @@
       <text font-family='sans-serif' font-size='10px' text-anchor='middle'>Zoom level</text>
       <g transform='translate(0,8)'>
         <path d='M -90 11 L 90 4 V 18 Z' fill='rgb(119, 119, 119)' />
-        <rect x=-90 y=0 height=22 :width="ratioToSliderPosScale().range()[1]-ratioToSliderPosScale().range()[0]" fill='rgb(255, 233, 103)' fill-opacity=0.5 stroke='rgb(255, 158, 0)' />
+        <rect
+          x=-90
+          y=0
+          height=22
+          :width="ratioToSliderPosScale().range()[1]-ratioToSliderPosScale().range()[0]"
+          fill='rgb(255, 233, 103)'
+          fill-opacity=0.5
+          stroke='rgb(255, 158, 0)'
+        />
         <text y=-3 x=-90 dominant-baseline='hanging' font-family='sans-serif' font-size='18px' fill='rgb(255, 158, 0)'>*</text>
-        <line y1=0 y2=22 :x1="posBasedOnRatio" :x2="posBasedOnRatio" stroke-width=2 stroke='black' />
+        <line
+          y1=0
+          y2=22
+          :x1="posBasedOnRatio"
+          :x2="posBasedOnRatio"
+          stroke-width=2
+          stroke='black'
+        />
         <line class='overlay' :ref="`${id}_overlay`" y1=11 y2=11 x1=-110 x2=110 stroke-width=24 stroke='transparent' cursor='ew-resize' />
       </g>
       <text transform='translate(0,50)' font-family='sans-serif' font-size='10px' text-anchor="middle" dominant-baseline="hanging" fill='rgb(255, 158, 0)'>
@@ -55,7 +70,7 @@ export default {
   },
   data() {
     return {
-      ntWidthInPixel: 0.5, // Updates --> minEfficiency per default, or user input
+      ntWidthInPixel: 1, // Updates --> minEfficiency per default, or user input
       zoomHasBeenSelected: false
     }
   },
@@ -85,8 +100,6 @@ export default {
   },
   created() {
     //Set default zoom value depending on computed
-    console.log('ntWidthInPixel is set');
-    console.log(this.acceptableNtToPxRatio);
     this.ntWidthInPixel = this.acceptableNtToPxRatio;
   },
   mounted() {
@@ -102,24 +115,34 @@ export default {
     this.centerContent();
   },
   watch: {
+    //Set current zoom level to default if needed
     getMinEfficiencyRatio: function() {
       //If zoom has been manually selected, auto-update do not apply
       if (!this.zoomHasBeenSelected) {
         this.ntWidthInPixel = this.getMinEfficiencyRatio
       }
     },
+    //update stored zoom level if any
     ntWidthInPixel: {
       immediate: true,
       handler: function() {
         this.updateGlobalZoom(this.ntWidthInPixel);
       }
     },
-    acceptableNtToPxRatio: function() {
-    },
-    lastNt: function() {
-    },
   },
   methods: {
+    convertToRatio(nbOfNtToSee) {
+      return this.displayWindowWidth / nbOfNtToSee
+    },
+    centerContent() {
+      let svgToMove = d3.select(this.$refs.zoomLegend);
+      let bboxSvg = svgToMove.node().getBBox();
+      let xMove = ((this.svgWidth - bboxSvg.width) / 2) + -bboxSvg.x;
+
+      svgToMove.attr('transform', `translate(${xMove},20)`);
+    },
+
+    //scale from zoom level to pos on slider
     ratioToSliderPosScale(value) {
       let self = this;
       let scale = d3.scaleLinear() //Linear Scale in two parts as low values are more important than "high" values that would not be displayed properly anyway, it has the number of features to display as an input
@@ -133,20 +156,12 @@ export default {
         return scale(value)
       }
     },
+    //Assign zoom value depending on pos on the slider
     updateNtToPxRatio(mousePos) {
       this.zoomHasBeenSelected = true;
       this.ntWidthInPixel = this.ratioToSliderPosScale().invert(mousePos);
     },
-    convertToRatio(nbOfNtToSee) {
-      return this.displayWindowWidth / nbOfNtToSee
-    },
-    centerContent() {
-      let svgToMove = d3.select(this.$refs.zoomLegend);
-      let bboxSvg = svgToMove.node().getBBox();
-      let xMove = ((this.svgWidth - bboxSvg.width) / 2) + -bboxSvg.x;
 
-      svgToMove.attr('transform', `translate(${xMove},20)`);
-    },
   }
 }
 </script>
