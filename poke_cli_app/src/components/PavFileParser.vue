255f522c57bda5762024bc8221eb428ed0f168c5
DURANT <durant@meerketeer.ird.fr>
2020-10-21 15:42:40 +0000

    Instanciated file parser

---
+++ poke_cli_app/src/components/PavFileParser.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <FileLoader :idBonus="'PavFile'" @file-loaded="doSomethingWithDataURL"/>
+
+</template>
+
+<script>
+import FileLoader from './components/FileLoader.vue';
+
+export default {
+  name: 'PavFileParser',
+  components: {
+    FileLoader,
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    self() {
+      return this;
+    }
+  },
+  methods: {
+    doSomethingWithDataURL: function(payload) {
+      console.log(payload.dataURL);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
