281b9130704b20e806ccd9c82de576acc8e553d4
Eloi Durant <eloi.durant@ird.fr>
2021-04-07 13:42:08 +0000

    Created a component for loading status

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-show="isLoading" class='text-center'>
+    <div class="spinner-border text-info" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+//import * as d3 from "d3";
+
+export default {
+  name: 'LoadingSpinner',
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    },
+
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  watch: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.spinner-border {
+  border: 0.4em solid currentColor;
+}
+
+</style>
