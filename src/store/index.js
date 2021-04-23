a1d47faf298f0b110052b7da203b1a728f07d156
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2021-04-23 11:53:18 +0200

    Masking of a part of HollowAreaFinder.vue when you put both parameters to 0 + comment added in the store

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
     optionPanelWidth: 300,
 
     displayIsLoading: false, //Whether a dataset is loading
-    fileLoaded: false,
+    fileLoaded: false, // Reflect if the main file is loaded
 
     genomeListInDisplay: [ 'Gen1', 'Gen2', 'Gen3', 'Gen4', 'Gen5', 'Gen6' ], //List of every genome name, same order as within the initial dataset
 
