63f756c497b8ad19f25a31233640f26b94063220
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2021-05-21 13:51:05 +0200

    In the component InputPosition.vue, you can now jump to an other chromosome + css added

--- src/components/InputPosition.vue
+++ src/components/InputPosition.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <b-input-group size="sm">
-      <!--<b-input-group-prepend>
-        <b-dropdown :text="selectedChrom !== null ? selectedChrom : 'Chr'" variant="success">
+      <b-input-group-prepend>
+        <b-dropdown size="sm" :text="selectedChrom !== null ? selectedChrom : 'Chr'">
           <b-dropdown-item v-for="chrom in chromList" :key="chrom" @click="updateSelectedChrom(chrom)">{{ chrom }}</b-dropdown-item>
         </b-dropdown>
-      </b-input-group-prepend>-->
+      </b-input-group-prepend>
       <b-form-input class="padLeft075" @keyup.enter="jumpToPosition(selectedNt)" v-model="selectedNt"></b-form-input>
       <b-input-group-append>
-        <b-button class="h31" size="sm" variant="outline-info" @click="jumpToPosition(selectedNt)">GO</b-button>
+        <b-button size="sm" class="h31 buttonPanache" @click="jumpToPosition(selectedNt)">GO</b-button>
       </b-input-group-append>
     </b-input-group>
   </div>
@@ -85,7 +85,9 @@ export default {
     jumpToPosition(destinationToReach) {
       if (destinationToReach !== undefined) {
         this.targetedPosNt = destinationToReach;
-        //this.updateSelectedChromStore(this.selectedChrom);
+        if (this.selectedChrom !== null) {
+          this.updateSelectedChromStore(this.selectedChrom);
+        }
       }
     },
   },
@@ -112,6 +114,66 @@ export default {
 
 .padLeft075 {
   padding-left: 0.75rem;;
+}
+
+.btn-secondary {
+  color: #495057;
+  background-color: #fff;
+  border-color: #ced4da;
+}
+
+.dropdown .b-dropdown .btn-group + button {
+  color: #495057;
+  background-color: #fff;
+  border: 1px solid #ced4da;
+  border-radius: .25rem;
+  transition: .2s;
+}
+
+</style>
+
+<style>
+
+.buttonPanache, .b-form-tags > .b-dropdown > .btn {
+  color: #495057;
+  background-color: #fff;
+  border: 1px solid #ced4da;
+  border-radius: .25rem;
+  transition: .2s;
+}
+
+.buttonPanache:hover, .b-form-tags > .b-dropdown > .btn:hover {
+  background-color: #e9ecef;
+  color: #495057;
+  border-color: #ced4da;
+}
+
+.buttonPanache:active, .b-form-tags > .b-dropdown > .btn:active {
+  color: #495057 !important;
+  background-color: #e9ecef !important;
+  border-color: #ced4da !important;
+}
+
+.buttonPanache:focus, .b-form-tags > .b-dropdown > .btn:focus {
+  color: #495057 !important;
+  background-color: #e9ecef !important;
+  border-color: #ced4da !important;
+}
+
+.input-group > .input-group-prepend > .btn-group > .btn {
+  color: #495057;
+  background-color: #fff;
+  border-color: #ced4da;
+}
+
+.input-group > .input-group-prepend > .btn-group > .btn:hover {
+  background-color: #e9ecef;
+}
+
+.input-group > .input-group-prepend > .btn-group > .btn:active {
+  color: #495057;
+  background-color: #e9ecef;
+  border-color: #ced4da;
 }
 
 </style>
